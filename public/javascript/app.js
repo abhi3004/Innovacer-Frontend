ac9014b6d0c3e5a56d9c8cc56d28d23503a84941
abhi3004 <abhijeet.pareek30@gmail.com>
2018-03-24 18:48:45 +0530

    sCommit

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -15,7 +15,7 @@ angular.module("app",['ngRoute','chart.js'])
     });
 })
 .controller("manageCtrl",manageCtrl)
-function manageCtrl($http,$window){
+function manageCtrl($http,$window,$scope,$timeout,$interval){
   var manage=this;
   manage.array=['fruit','vegetable','pulses'];
   manage.currency=['INR(₹)','U.S. Dollar($)','Pound(£)','Euro(€)','Yen(¥)'];
@@ -30,9 +30,22 @@ function manageCtrl($http,$window){
     manage.sel_curr="";
   };
   manage.addexpenses=function(a,b,c,d,e){
-    manage.total1=a+b+c+d+e;
+    
+    manage.total=a+b+c+d+e;
     console.log(manage.total);
+    
   }
-  manage.labels=["Food", "Transport", "Grocery"];
-  manage.data = [300, 500, 100];
+ 
+  manage.labels=["Food", "Transport", "Grocery","Entertainment","Misclleneous"];
+  manage.colors=["rgb(255,0,0)","rgb(0,100,0)","rgb(255,165,0)","rgb(255,215,0)","rgb(238,130,238)"];
+  $interval(function () {
+  manage.data = [];
+  manage.data[0]=manage.food;
+  manage.data[1]=manage.transport;
+  manage.data[2]=manage.grocery;
+  manage.data[3]=manage.entertainment;
+  manage.data[4]=manage.misc;
+  
+}, 3000);
+  
 }
